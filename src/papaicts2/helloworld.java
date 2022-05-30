c0f94d771db64a698ad6facb296a88a1b2bb2003
papaicts <papaishee8@gmail.com>
2022-05-30 10:06:58 +0530

    first commit

---
+++ src/papaicts2/helloworld.java
@@ -0,0 +1,12 @@
+package papaicts2;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO PAPAI");
+		System.out.println("hey pkpk");
+		System.out.println("okok");
+	}
+
+}
